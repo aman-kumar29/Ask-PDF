795da5b905a7758db58495b3f4cc76953506e5b9
Aman <amankumar76814@gmail.com>
2024-06-25 09:55:53 +0530

    corrected code

--- llm.py
+++ llm.py
@@ -1,11 +1,10 @@
 from langchain.document_loaders import PyPDFLoader
-
 from langchain.vectorstores import Chroma
-from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
 from langchain import hub
-from langchain.chat_models import ChatOpenAI
-from langchain.schema.runnable import RunnablePassthrough
-
+from langchain.llms import GooglePalm
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
 
 def load_pdf_and_split(path):
     loader = PyPDFLoader(path)
@@ -14,19 +13,25 @@ def load_pdf_and_split(path):
 
 
 def embed_and_store_splits(splits):
-    vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+    vectorstore = Chroma.from_documents(documents=splits, embedding=HuggingFaceEmbeddings())
     return vectorstore
-
 
 def queryPDF(vectorstore, query):
     retriever = vectorstore.as_retriever()
     rag_prompt = hub.pull("rlm/rag-prompt")
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+    llm = GooglePalm(google_api_key=palm_api_key, temperature=0.2)
+
+    def format_docs(docs):
+        return "\n\n".join(doc.page_content for doc in docs)
 
     rag_chain = (
-        {"context": retriever, "question": RunnablePassthrough()} | rag_prompt | llm
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        | rag_prompt
+        | llm
+        | StrOutputParser()
     )
+
     print("Querying... ", query)
     response = rag_chain.invoke(query)
     print("Response: ", response)
-    return response.content
+    return response
